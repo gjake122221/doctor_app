40e095703d6b85571ac7b7635ebe4abe06be64dc
Jake Cabreros <rjake909@gmail.com>
2023-10-07 20:48:44 +0800

    add data list

---
+++ lib/data/data_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+var doctors = [
+  {
+    'name': 'Selma Sigi',
+    'doc': 'Pediatrician',
+    'hos': 'Silver Pine Community Hospital',
+    'exp': '12',
+    'ps': '1604',
+    'img': 'assets/image/Group_22.png'
+  },
+  {
+    'name': 'Gerold Meike',
+    'doc': 'Family medicine',
+    'hos': 'Great Plains Medical Center',
+    'exp': '9',
+    'ps': '1450',
+    'img': 'assets/image/Group_23.png'
+  },
+  {
+    'name': 'Utz Corinna',
+    'doc': 'Pediatrician',
+    'hos': 'Crossroads Community Hospital',
+    'exp': '7',
+    'ps': '1309',
+    'img': 'assets/image/Group_24.png'
+  },
+  {
+    'name': 'Melitta Wendel',
+    'doc': 'Surgeon',
+    'hos': 'Beacon Medical Center',
+    'exp': '10',
+    'ps': '1497',
+    'img': 'assets/image/Group_25.png'
+  },
+];
+
+List<Widget> btnicons = [
+  Image.asset('assets/icons/007_stethoscope.png'),
+  Image.asset('assets/icons/004_teeth.png'),
+  Image.asset('assets/icons/017_heart.png'),
+  Image.asset('assets/icons/024_clinic.png'),
+  Image.asset('assets/icons/012_medicine.png'),
+  Image.asset('assets/icons/013_care_2.png'),
+  Image.asset('assets/icons/028_bandage.png'),
+  Image.asset('assets/icons/005_syringe.png'),
+];
+
+List<Widget> othericons = [
+  Image.asset('assets/icons/Icons_Artboard.png'),
+  Image.asset('assets/icons/Icons_Back.png'),
+  Image.asset('assets/icons/Icons_Comment.png'),
+  Image.asset('assets/icons/Icons_Menu_Burger.png'),
+  Image.asset('assets/icons/Icons_Star.png'),
+  Image.asset('assets/icons/Search.png'),
+];
